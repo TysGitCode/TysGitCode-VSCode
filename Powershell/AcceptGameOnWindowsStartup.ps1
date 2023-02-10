ecfea3e1e0713efbb3ef19e957e5f20e54965dc9
Ty <tmitche5@uccs.edu>
2023-02-09 23:58:33 -0700

    commit

---
+++ Powershell/AcceptGameOnWindowsStartup.ps1
@@ -0,0 +1,18 @@
+#Goes to drive location
+cd D:
+#Goes to script location in vs code
+cd TysGitCode-VSCode\Python\LeagueProjects\AcceptMatchScript
+
+
+#Imports pywin32
+pip install pywin32
+#Imports Pyautogui
+pip install pyautogui
+#Imports Time
+pip install Time
+#Imports open cv
+pip install opencv-python
+
+
+#Runs Python file
+python AcceptGameV2.py
