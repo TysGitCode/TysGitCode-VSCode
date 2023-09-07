192b45c9d8b9afee8d69bf92c89528a6152c32f0
Ty <tmitche5@uccs.edu>
2023-09-07 02:35:44 -0600

    done

--- C_Personal/Powershell/League/AcceptGameV2.cmd
+++
@@ -1,2 +0,0 @@
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell C:\Users\xsnot\OneDrive\Desktop\Scripts\2_StartAcceptGameV2Script.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
