ce9d66536e20694f31a90f6bf8830bb662c5f4bc
Ty Mitchell <tmitche5@uccs.edu>
2023-10-04 18:24:59 -0600

    links working

--- Code/Testing/practice.py
+++ Code/Testing/practice.py
@@ -1,6 +1,11 @@
 import re
 from pdfminer.high_level import extract_pages, extract_text
+from pdfminer.layout import LTTextBox
+from pdfminer.layout import LTText
+from pdfminer.layout import LTChar
 
-for page_layout in extract_pages("D:/TysGitCode-VSCode/Code/Testing/Images/Nov2022Statement-2022-11.pdf"):
+for page_layout in extract_pages("C:/Users/tmitche5/TysGitCode-VSCode/Code/Testing/Images/Nov2022Statement-2022-11.pdf"):
     for element in page_layout:
-        print(element)
+        if isinstance(element, LTTextBox):
+            for text_line in element:
+                re.findall("''", text_line)
