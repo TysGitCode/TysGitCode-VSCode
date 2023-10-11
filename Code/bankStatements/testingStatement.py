481ed63d7a1e667e63018183fed8a51021625ff3
Ty <tmitche5@uccs.edu>
2023-10-11 01:01:33 -0600

    testing for statement script

---
+++ Code/bankStatements/testingStatement.py
@@ -0,0 +1,33 @@
+import re
+
+import parse
+import pdfplumber
+import pandas as pd
+from collections import namedtuple
+Line = namedtuple('Line', 'Trans_Date, Eff_Date, Transaction_Description, Amount, Balance')
+
+company_re = re.compile(r'(V\d+) (.*) Phone:')
+line_re = re.compile(r'\d{2}/\d{2}/\d{4} \d{2}/\d{2}/\d{4}')
+file = 'Sample Report Pythonic.pdf'
+lines = []
+total_check = 0
+
+with pdfplumber.open(file) as pdf:
+    pages = pdf.pages
+    for page in pdf.pages:
+        text = page.extract_text()
+        for line in text.split('\n'):
+            print(line)
+            comp = company_re.search(line)
+            if comp:
+                vend_no, vend_name = comp.group(1), comp.group(2)
+
+            elif line.startswith('INVOICES'):
+                doctype = 'INVOICE'
+
+            elif line.startswith('CREDITNOTES'):
+                doctype = 'CREDITNOTE'
+
+            elif line_re.search(line):
+                items = line.split()
+                lines.append(Line(vend_no, vend_name, doctype, *items))
