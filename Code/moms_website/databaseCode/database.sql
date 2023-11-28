cb152537d71d7592df7acda4693f14d8e98963f4
Ty <tmitche5@uccs.edu>
2023-11-28 11:02:46 -0700

    test

---
+++ Code/moms_website/databaseCode/database.sql
@@ -0,0 +1,205 @@
+create database momsWebsite;
+use momsWebsite;
+
+create table aboutmemain (
+    content varchar (255)
+);
+
+insert into aboutmemain (content)
+values ("placeholder");
+
+
+
+create table family (
+    content varchar (255)
+);
+
+insert into family (content)
+values ("placeholder");
+
+
+
+create table hobbies (
+    content varchar (255)
+);
+
+insert into hobbies (content)
+values ("placeholder");
+
+
+
+create table education (
+    content varchar (255)
+);
+
+insert into education (content)
+values ("placeholder");
+
+
+
+create table experience (
+    content varchar (255)
+);
+
+insert into experience (content)
+values ("placeholder");
+
+
+
+create table recognition (
+    content varchar (255)
+);
+
+insert into recognition (content)
+values ("placeholder");
+
+
+
+create table note1 (
+    content varchar (255)
+);
+
+insert into note1 (content)
+values ("placeholder");
+
+
+
+create table note2 (
+    content varchar (255)
+);
+
+insert into note2 (content)
+values ("placeholder");
+
+
+
+create table note3 (
+    content varchar (255)
+);
+
+insert into note3 (content)
+values ("placeholder");
+
+
+
+create table homework1 (
+    content varchar (255)
+);
+
+insert into homework1 (content)
+values ("placeholder");
+
+
+
+create table homework2 (
+    content varchar (255)
+);
+
+insert into homework2 (content)
+values ("placeholder");
+
+
+
+create table curriculum1 (
+    content varchar (255)
+);
+
+insert into curriculum1 (content)
+values ("placeholder");
+
+
+
+create table curriculum2 (
+    content varchar (255)
+);
+
+insert into curriculum2 (content)
+values ("placeholder");
+
+
+
+create table curriculum3 (
+    content varchar (255)
+);
+
+insert into curriculum3 (content)
+values ("placeholder");
+
+
+
+create table curriculum4 (
+    content varchar (255)
+);
+
+insert into curriculum4 (content)
+values ("placeholder");
+
+
+
+create table curriculum5 (
+    content varchar (255)
+);
+
+insert into curriculum5 (content)
+values ("placeholder");
+
+
+
+create table management1 (
+    content varchar (255)
+);
+
+insert into management1 (content)
+values ("placeholder");
+
+
+
+create table management2 (
+    content varchar (255)
+);
+
+insert into management2 (content)
+values ("placeholder");
+
+
+
+create table management3 (
+    content varchar (255)
+);
+
+insert into management3 (content)
+values ("placeholder");
+
+
+
+create table managementcont1 (
+    content varchar (255)
+);
+
+insert into managementcont1 (content)
+values ("placeholder");
+
+
+
+create table managementcont2 (
+    content varchar (255)
+);
+
+insert into managementcont2 (content)
+values ("placeholder");
+
+
+
+create table managementcont3 (
+    content varchar (255)
+);
+
+insert into managementcont3 (content)
+values ("placeholder");
+
+create table communication (
+    content varchar (255)
+);
+
+insert into communication (content)
+values ("placeholder");
