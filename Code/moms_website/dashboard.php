5040326b6fe235a6f6298a889d59b36f9721d8e3
Ty Mitchell <tmitche5@uccs.edu>
2023-11-29 17:56:15 -0700

    testing

--- Code/moms_website/dashboard.php
+++ Code/moms_website/dashboard.php
@@ -10,24 +10,60 @@ if (!isset($_SESSION['user']) || $_SESSION['user'] !== 'your_mother_username') {
 // Include the database connection
 include 'connect.php';
 
-// Fetch all rows from the database
-$sql = "SELECT * FROM aboutmemain";
-$result = $conn->query($sql);
+// Define an array of tables
+$tables = array(
+    'aboutmemain',
+    'curriculum3',
+    'curriculum4',
+    'curriculum5',
+    'education',
+    'experience',
+    'family',
+    'hobbies',
+    'homework1',
+    'homework2',
+    'management1',
+    'management2',
+    'management3',
+    'managementcont1',
+    'managementcont2',
+    'managementcont3',
+    'note1',
+    'note2',
+    'note3',
+    'recognition'
+);
 
-// Check if the query was successful
-if ($result) {
-    // Check if data is fetched successfully
-    if ($result->num_rows > 0) {
-        // Output data of the first row
-        $row = $result->fetch_assoc();
-        $existingContent = isset($row['content']) ? $row['content'] : '';
+// Loop through the tables
+foreach ($tables as $table) {
+    // Fetch content from each table
+    $sql = "SELECT * FROM $table";
+    $result = $conn->query($sql);
+
+    // Check if the query was successful
+    if ($result) {
+        // Check if data is fetched successfully
+        if ($result->num_rows > 0) {
+            // Output data of the first row
+            $row = $result->fetch_assoc();
+            $existingContent = isset($row['content']) ? $row['content'] : '';
+        } else {
+            echo "Error: No data found for table $table.";
+            $existingContent = ''; // Set a default value if no data is found
+        }
     } else {
-        echo "Error: No data found.";
-        $existingContent = ''; // Set a default value if no data is found
+        echo "Error executing query: " . $conn->error;
+        $existingContent = ''; // Set a default value in case of an error
     }
-} else {
-    echo "Error executing query: " . $conn->error;
-    $existingContent = ''; // Set a default value in case of an error
+
+    // Display a form for each table
+    echo "<h3>$table</h3>";
+    echo "<form method='post' action='update_content.php'>";
+    echo "<textarea name='updated_content'>$existingContent</textarea>";
+    echo "<input type='hidden' name='table_name' value='$table'>";
+    echo "<input type='hidden' name='redirect_url' value='$_SERVER[REQUEST_URI]'>";
+    echo "<button type='submit'>Update Content</button>";
+    echo "</form><br>";
 }
 
 // Close the database connection
@@ -45,12 +81,6 @@ $conn->close();
 
 <body>
     <h2>Dashboard</h2>
-    <form method="post" action="update_content.php">
-        <textarea name="updated_content"><?php echo $existingContent; ?></textarea>
-        <input type="hidden" name="redirect_url" value="<?php echo $_SERVER['REQUEST_URI']; ?>">
-        <button type="submit">Update Content</button>
-    </form>
-    <br>
     <a href="logout.php">Logout</a>
 </body>
 
---
+++ Code/moms_website/database.sql
@@ -0,0 +1,30 @@
+-- Create table
+CREATE TABLE boxcontents (
+    aboutmemain VARCHAR(500),
+    communication VARCHAR(500),
+    curriculum1 VARCHAR(500),
+    curriculum2 VARCHAR(500),
+    curriculum3 VARCHAR(500),
+    curriculum4 VARCHAR(500),
+    curriculum5 VARCHAR(500),
+    education VARCHAR(500),
+    experience VARCHAR(500),
+    family VARCHAR(500),
+    hobbies VARCHAR(500),
+    homework1 VARCHAR(500),
+    homework2 VARCHAR(500),
+    management1 VARCHAR(500),
+    management2 VARCHAR(500),
+    management3 VARCHAR(500),
+    managementcont1 VARCHAR(500),
+    managementcont2 VARCHAR(500),
+    managementcont3 VARCHAR(500),
+    note1 VARCHAR(500),
+    note2 VARCHAR(500),
+    note3 VARCHAR(500),
+    recognition VARCHAR(500)
+);
+
+-- Insert data
+INSERT INTO boxcontents (aboutmemain, communication, curriculum1, curriculum2, curriculum3, curriculum4, curriculum5, education, experience, family, hobbies, homework1, homework2, management1, management2, management3, managementcont1, managementcont2, managementcont3, note1, note2, note3, recognition) 
+VALUES ('test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test');
