ebf852e11462d05652e1aa1ab2e411b2e077056e
Ty <tmitche5@uccs.edu>
2024-01-13 22:27:31 -0700

    test2

--- DetailingSite/messages.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS contact_form;
-USE contact_form;
-
-CREATE TABLE IF NOT EXISTS contact_entries (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    message TEXT NOT NULL
-);
